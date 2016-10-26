e267b422e5de36e862b18f815ed1134aec4d9f8c
deprim1 <rekfan00@gmail.com>
2016-10-26 09:47:45 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,221 +1,112 @@
-<?php 
-session_start(); 
-require_once("includes/connection.php");
-if(isset($_POST["login"])){ 
-if(!empty($_POST['username']) && !empty($_POST['password'])) { 
-$username=htmlspecialchars($_POST['username']); 
-$password=htmlspecialchars($_POST['password']); 
-$query =mysql_query("SELECT * FROM usertbl WHERE username='".$username."' AND password='".$password."'"); 
-$numrows=mysql_num_rows($query); 
+<?php require_once("includes/header.php"); ?>
+<?php
+	if(isset($_POST["register"])){
+	if(!empty($_POST['full_name']) && !empty($_POST['email']) && !empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['full_prizv']) &&
+		!empty($_POST['telephone']) && !empty($_POST['passwordd'])) {
+  $full_name= htmlspecialchars($_POST['full_name']);
+	$email=htmlspecialchars($_POST['email']);
+ $username=htmlspecialchars($_POST['username']);
+ $password=htmlspecialchars($_POST['password']);
+ $passwordd=htmlspecialchars($_POST['passwordd']);
+  $full_prizv=htmlspecialchars($_POST['full_prizv']);
+  $telephone=htmlspecialchars($_POST['telephone']);
+ 
 
-if($numrows!=0) 
-{ 
-while($row=mysql_fetch_assoc($query)) 
-{ 
-$dbusername=$row['username']; 
-$dbpassword=$row['password']; 
-} 
-if($username == $dbusername && $password == $dbpassword) 
-{ 
-
-$_SESSION['session_username']=$username; 
-/* Перенаправление браузера */ 
-header("Location: profile.php"); 
-} 
-} else { 
-echo "Неправильний логін або пароль. Введіть заново."; 
-} 
-} else { 
-$message = "Всі поля захищені!"; 
-} 
-} 
-?> 
-
-<!DOCTYPE html>
-<html>
- <section id="vse">
-    <head>
-            <meta charset="utf-8">
-                <title>Книгообмін Хмельницький</title>
-                     <?php require_once("includes/connection.php"); ?>
-                    <link href="css/style1.css" media="screen" rel="stylesheet">
-                    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'rel='stylesheet' type='text/css'>
-                    <link rel="Stylesheet" href="StyleCSS/style.css">
-                    <link rel="Stylesheet" href="StyleCSS/menu.css">
-                    <link href="StyleCSS/bootstrap.min.css" rel="stylesheet">
-                    <link href="StyleCSS/bootstrap-theme.min.css" rel="stylesheet">
-                    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-                    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-                    <script type="text/javascript" src="jquary/jquery-2.1.4.min.js"></script> 
-                    <script type="text/javascript" src="jquary/jquery-ui.js"></script>
-                    <script type="text/javascript" src="jav.js"></script>
-                    <script type="text/javascript" src="js/main.js"></script> 
-    </head>
-
-<body>
-        <header>
-
-   <div class="navbar-wrapper">
-      <div class="container">
-
-<section id="menu">
-        <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="index.php">BookSwap</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                <li><a href="index.php"><img id="imgurl" src="images/main_page.jpg" alt="Головна сторінка"></a></li>
-                <li><a class="menuha" href="reg.php">Реєстрація</a></li>
-                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Книги<span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="show.php">Перегляд</a></li>
-                    <li><a href="add.php">Добавлення</a></li>
-                  </ul>
-                </li>
-                <li> 
-                <section id="lk"> 
-                <?php if ($_SESSION['session_username']!="") { 
-                echo "<a class='menuha1' href='profile.php'>".$_SESSION['session_username']."</a>"."<br>"; 
-                echo "<span><a class='menuha1' href='logout.php'>Вийти</a></span>"; 
-                }
-                ?> 
-                </section> 
-                </li> 
-              </ul>
-            </div>
-          </div>
-        </nav>
-</section>
-
-      </div>
-    </div>
-
-
-        </header>
-
-            <section id="login_form">
-
-    <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-          <h2 class="featurette-heading">
-            <style type="text/css">
-                    #login_form {
-                        background: url(images/main.jpg);
-                        background-size: 100% 100%;
-                    }
-                </style>
-                <section id="logo">
-                    <h1>Книгообмін</h1>
-                        <section id="s1">у Хмельницькому</section id="s1"> <br>
-                        <section id="s2">Залиш свою пропозицію та переглянь уже запропоновані :)</section id="s2">
-                </section></h2>
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                  <form class="form-horizontal" action="" id="loginform" method="post" name="loginform">
-                    <table>
-                        <div id="login">
-                            <h2>LogIn</h2>
-                                <div class="form-group">
-                                    <label for="user_login" class="col-xs-2 col-sm-2 col-md-2 col-lg-2 control-label">Логін</label>
-                                        <div class="col-xs-2 col-sm-4 col-md-4 col-lg-10">
-                    <input name="username" size="20" value="" type="text" class="form-control" id="username" placeholder="Логін">
-                                        </div>
-                                </div>                                    
-
-                                <div class="form-group">
-                                    <label for="user_pass" class="col-xs-2 col-sm-2 col-md-2 col-lg-2 control-label">Пароль</label>
-                                        <div class="col-xs-2 col-sm-4 col-md-4 col-lg-10">
-                    <input id="password" name="password" type="password" size="20" class="form-control" value="" placeholder="Пароль">
-                                        </div>
-                                </div> 
-
-                                <div class="form-group">
-                        <div class="col-sm-offset-2 col-xs-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-2 col-sm-4 col-md-4 col-lg-10">
-                        <button id="submit" name="login" type="submit" class="btn btn-primary btn-lg btn-block btn-success">Ввійти</button>
-                                    </div>
-                                </div>
-                                <p class="regtext">Ще не зареєстувалися? <a href= "reg.php">Реєстрація</a>!</p>
-                        </div>
-                    </table> 
-            </form>      
-        </div>
-
-        </div>  
-
-            </section>
-
-<section id="main">
-    <section id="content">
-
-<section id="vk">
-   <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-5">
-            <a href="https://vk.com/knigoobmin">
-        <img id="img_vk" class="featurette-image img-responsive" src="images/vk.jpg" alt="Офіційна сторінка ВК: Книгообмін Хмельницький">
-            </a>
-        </div>
-        <div class="col-md-7">
-          <h2 class="featurette-heading">О так! Це добре. <span class="text-muted">Дивіться самі.</span></h2>
-          <p class="lead">У нашого сайту є офіційна спільнота в соціальній мережі "Вконтакте". Натиснувши на картинку ви зможете перейти на сторінку спільноти, де можете переглянути додаткові новини та пости від користувачів мережі.</p>
-        </div>
-      </div>
-</section>
-
-        <div class="container header">
-            <div class="container-fluide">
-
-            <h1 class="text-uppercase">Чому саме ми?</h1>
-                <div class="row text-uppercase" >
-
-            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <span class="glyphicon glyphicon-search"></span>
-              <p class="ti">Вільний доступ</p>
-            </div>
-
-            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <span class="glyphicon glyphicon-heart"></span>
-              <p class="ti">Нас люблять</p>
-            </div>
-
-            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <span class="glyphicon glyphicon-thumbs-up"></span>
-              <p class="ti">Якісний сервіс</p>
-            </div>
-
-            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <span class="glyphicon glyphicon-sunglasses"></span>
-              <p class="ti">Ми просто круті</p>
-            </div>
-
-            </div>
-        </div>                    
-                         
-    </section>
-</section>
+	$query = ("SELECT id FROM usertbl WHERE username='$username'");
+            $sql = mysql_query($query) or die(mysql_error());
             
-<footer>
-    <a href="#top">Наверх</a><br>
-        <address>(c) IT-бобри, Україна, Хмельницький, вул.Зарічанська 10/2</address>
-</footer>
+            if (mysql_num_rows($sql) > 0) {
+                echo '<font color="red">Пользователь с таким логином зарегистрирован!</font>';
+            }
+            else {
+                $query2 = ("SELECT id FROM usertbl WHERE email='$email'");
+                $sql = mysql_query($query2) or die(mysql_error());
+                if (mysql_num_rows($sql) > 0){
+                    echo '<font color="red">Пользователь с таким e-mail уже зарегистрирован!</font>';
+                }
+                else{
+                    $query = "INSERT INTO usertbl (full_name, email, username, password, full_prizv, telephone )
+                              VALUES ('$full_name','$email','$username','$password' ,'$full_prizv','$telephone')";
+                    $result = mysql_query($query) or die(mysql_error());;
+                    echo '<font color="green"> Вы успешно зарегистрировались!</font><br>';
+                    
+                                
+                }
+            }
+	}
+	}
+	?>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/docs.min.js"></script>
+	<?php if (!empty($message)) {echo "<p class=\"error\">" . "MESSAGE: ". $message . "</p>";} ?>
+  <section>
+<html>
+<head>
+  <link rel="stylesheet" href="Register.css">
+    
+    <script src="jquery-2.1.4.min.js"></script>
 
+  <script src="jquery.maskedinput.min.js"></script>
+    
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+</head>
+<header>
+      <center><H1><label>Реєстрація на сайті</label></H1></center>
+      
+      
+
+      </header>
+<body>
+		
+                    <section>
+          <form action="index.php" id="registerform" method="post"name="registerform">
+
+           <div class=dani>
+
+      <article>
+      <h3><b><label>Введіть свої дані</label></b></h3>
+<table>
+
+  <p><label for="user_login">Ім'я</label>
+ <input class="input" id="full_name" name="full_name"size="32"  type="text" value="" pattern="[А-Я а-я Є є Ї ї І і]{2,}" title="Введіть свою область" required></label></p>
+
+
+<p><label for="user_login">Прізвище</label>
+ <input class="input" id="full_prizv" name="full_prizv"size="32"  type="text" value="" pattern="[А-Я а-я Є є Ї ї І і]{2,}" title="Введіть свою область" required></label></p>
+
+
+<p><label for="user_pass">Мобільний номер</label>
+<input class="input" id="telephone" name="telephone"size="32"   type="text" value=""></label></p>
+<script>
+        $(function(){
+          $("#phone").mask("+38(999) 999-99-99");
+        });
+        </script>
+
+<p><label for="user_pass">E-mail</label>
+<input class="input" id="email" name="email" size="32"type="email"  value=""></label></p>
+
+
+<p><label for="user_pass">Логін</label>
+<input class="input" id="username" name="username"size="32" type="text" value=""></label></p>
+
+
+<p><label for="user_pass">Пароль</label>
+<input class="input" id="password" name="password"size="32"   type="password" value=""pattern="{4,15}" title="Пароль повинен мати 4-12 символів" required></p>
+
+
+<p><label for="user_pass">Повторіть пароль</label>
+<input class="input" id="passwordd" name="passwordd"size="32"   type="password" value="" pattern="{4,15}" title="Пароль повинен мати 4-12 символів" required></p>
+
+<label>Я прочитав правила сайту </label> <br>
+      <label>i погоджуюсь з ними.</label><input type ="checkbox" required >
+      <br>
+ <p class="submit"><input class="button" id="submit" name= "register" type="submit" value="Зареєструватися"></p>
+	  <p class="regtext">Вже зареєстровані? <a href= "logout.php">Війти в систему</a>!</p>
+</table>
+            </form>
+              					
+</div>
 </body>
-</section>
 </html>
+  </div>
--- profile.php
+++ profile.php
@@ -4,98 +4,30 @@ include_once("includes/connection.php");
  $username= $_SESSION['session_username'];
 $resultat = mysql_query("SELECT * FROM usertbl WHERE username='$username'");
 $array = mysql_fetch_array($resultat);
-if(!isset($_SESSION["session_username"])){
-header("Location: index.php");
-} 
 ?>
-
-<!DOCTYPE html>
-<html>
- <section id="vse">
-<head>
-            <meta charset="utf-8">
-                <title>Книгообмін Хмельницький</title>
-                     <?php require_once("includes/connection.php"); ?>
-                    <link href="css/style1.css" media="screen" rel="stylesheet">
-                    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'rel='stylesheet' type='text/css'>
-                    <link rel="Stylesheet" href="StyleCSS/style.css">
-                    <link rel="Stylesheet" href="StyleCSS/menu.css">
-                    <link href="StyleCSS/bootstrap.min.css" rel="stylesheet">
-                    <link href="StyleCSS/bootstrap-theme.min.css" rel="stylesheet">
-                    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-                    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-                    <script type="text/javascript" src="jquary/jquery-2.1.4.min.js"></script> 
-                    <script type="text/javascript" src="jquary/jquery-ui.js"></script>
-                    <script type="text/javascript" src="jav.js"></script>
-                    <script type="text/javascript" src="js/main.js"></script> 
-    </head>
-
-<body>
-        <header>
-
-   <div class="navbar-wrapper">
-      <div class="container">
-
-<section id="menu">
-        <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="index.php">BookSwap</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                <li><a href="index.php"><img id="imgurl" src="images/main_page.jpg" alt="Головна сторінка"></a></li>
-                <li><a class="menuha" href="reg.php">Реєстрація</a></li>
-                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Книги<span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="show.php">Перегляд</a></li>
-                    <li><a href="add.php">Добавлення</a></li>
-                  </ul>
-                </li>
-                <li> 
-                <section id="lk"> 
-                <?php if ($_SESSION['session_username']!="") { 
-                echo "<a class='menuha1' href='profile.php'>".$_SESSION['session_username']."</a>"."<br>"; 
-                echo "<span><a class='menuha1' href='logout.php'>Вийти</a></span>"; 
-                }
-                ?> 
-                </section> 
-                </li> 
-              </ul>
-            </div>
-          </div>
-        </nav>
+<?php include("includes/header.php"); ?>
+<section class="session">
+<p>Добро пожаловать, <span><?php echo $_SESSION['session_username'];?>! </p>
+<p></span> <a  id="out" href="profile.php"> Особистий кабінет</a></p>
+<p></span> <a  id="out" href="logout.php"> Вийти</a> з системи</p>
 </section>
+   <html lang="en-GB">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link href="styles.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="style.css" >    
+    
+<link href="css/screen.css" rel="stylesheet" type="text/css" media="screen" />
+<script type="text/javascript" src="jq.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 
-      </div>
-    </div>
-
-
-        </header>
-
+</head>
 <body>
 
-<!-- header begins -->
-<div id="header">
-<div id="head_right">
-    
-</div>    
-<div id="head_left">
-<div id="logo"><a href="#"></a>      
-</div>
 
 <h1>Вітаємо вас на нашому сайті!</h1> 
     <center>
         <div id="main">
-<!-- header ends -->
-<!-- content begins -->
   <div id="content">
     
             <div id="left">
@@ -122,12 +54,3 @@ $result = mysql_query("SELECT * FROM usertbl WHERE username='$username'");
       
 ?>
 </center>
-
-<footer>
-    <a href="#top">Наверх</a><br>
-        <address>(c) IT-бобри, Україна, Хмельницький, вул.Зарічанська 10/2</address>
-</footer>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/docs.min.js"></script>
